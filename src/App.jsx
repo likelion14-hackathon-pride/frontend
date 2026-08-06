6be021315eb15c4ac158b9c775bb46ad27b4bba2
do0hetfield <cjhjoonh77@icloud.com>
2026-08-07 06:27:15 +0900

    [feat] 온보딩 위저드 뼈대 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// App.jsx (BrowserRouter 빼기)
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import OnboardingOwner from './pages/OnboardingOwner'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/onboarding/owner" element={<OnboardingOwner />} />
     </Routes>
   )
 }
