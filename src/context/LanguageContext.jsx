3a4426abca100315a6a4712f240ba498741fb0f2
ymnnnn <ymoh0713@gmail.com>
2026-08-07 01:31:54 +0900

    [feat] 로그인 화면 구현

---
+++ src/context/LanguageContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from 'react'
+
+const LanguageContext = createContext(null)
+
+export const LanguageProvider = ({ children }) => {
+  const [lang, setLang] = useState('ko') 
+
+  return (
+    <LanguageContext.Provider value={{ lang, setLang }}>
+      {children}
+    </LanguageContext.Provider>
+  )
+}
+
+export const useLanguage = () => useContext(LanguageContext)
